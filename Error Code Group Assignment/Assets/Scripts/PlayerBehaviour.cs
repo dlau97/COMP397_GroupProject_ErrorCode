a233fbe0690a2454eac8c9f481fd755ee92e73f0
GearShark123 <lylespurrell@hotmail.com>
2021-05-31 15:15:13 -0400

    Added mech dashing ability

--- Error Code Group Assignment/Assets/Scripts/PlayerBehaviour.cs
+++ Error Code Group Assignment/Assets/Scripts/PlayerBehaviour.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    //public float movementForce, jumpForce;
-    //public Rigidbody rigidBody;
+
     public CharacterController controller;
     public bool isGrounded;
+    public bool isDashing;
     public float groundRadius = 0.5f;
     public float maxSpeed = 10.0f;
     public float gravity = -30.0f;
     public float jumpHeight = 3.0f;
+    public float dashForce = 30.0f;
     public Transform groundCheck;
     public LayerMask groundMask;
     public Vector3 velocity;
+    public Vector3 move;
+    public Vector3 dashDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        //rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -36,9 +38,24 @@ public class PlayerBehaviour : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        if (isDashing == false)
+        {
+            move = transform.right * x + transform.forward * z;
+            controller.Move(move * maxSpeed * Time.deltaTime);
+        }
 
-        controller.Move(move * maxSpeed * Time.deltaTime);
+        if (isDashing == false && Input.GetButtonUp("Left Shift") && isGrounded && move != new Vector3(0,0,0))
+        {
+            Debug.Log("Dash");
+            dashDirection = move;
+            Dash();
+            Invoke("Dash", 1);
+        }     
+
+        if (isDashing == true)
+        {
+            controller.Move(dashDirection * dashForce * Time.deltaTime);            
+        }
 
         if (Input.GetButton("Jump") && isGrounded)
         {
@@ -46,65 +63,24 @@ public class PlayerBehaviour : MonoBehaviour
         }
 
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
-
-        //if (isGrounded)
-        //{
-        //    if (Input.GetAxisRaw("Horizontal") > 0)
-        //    {
-        //        rigidBody.AddForce(Vector3.right * movementForce);
-        //    }
-
-        //    if (Input.GetAxisRaw("Horizontal") < 0)
-        //    {
-        //        rigidBody.AddForce(Vector3.left * movementForce);
-        //    }
-
-        //    if (Input.GetAxisRaw("Vertical") > 0)
-        //    {
-        //        rigidBody.AddForce(Vector3.forward * movementForce);
-        //    }
-
-        //    if (Input.GetAxisRaw("Vertical") < 0)
-        //    {
-        //        rigidBody.AddForce(Vector3.back * movementForce);
-        //    }
-
-        //    if (Input.GetAxisRaw("Jump") > 0)
-        //    {
-        //        rigidBody.AddForce(Vector3.up * jumpForce);
-        //    }
-        //}
     }
-
-    //void OnCollisionEnter(Collision other)
-    //{
-    //    if (other.gameObject.CompareTag("Ground"))
-    //    {
-    //        isGrounded = true;
-    //    }
-    //}
-
-    //void OnCollisionStay(Collision other)
-    //{
-    //    if (other.gameObject.CompareTag("Ground"))
-    //    {
-    //        isGrounded = true;
-    //    }
-    //}
-
-    //void OnCollisionExit(Collision other)
-    //{
-    //    if (other.gameObject.CompareTag("Ground"))
-    //    {
-    //        isGrounded = false;
-    //    }
-    //}
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+
+    void Dash()
+    {
+        if (isDashing == true)
+        {
+            isDashing = false;
+        }
+        else
+        {
+            isDashing = true;
+        }
     }
 }
